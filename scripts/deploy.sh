b82e077476eb1cda35b8bbb3ea2c584a94412924
Hezlepinc <john@hezlepinc.com>
2025-10-26 11:34:32 -0400

    Deploy: use remote C:\Users\john for Cloudways paths to avoid /home perms

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -46,10 +46,11 @@ TARGET_DOMAIN="$DOMAIN"
 echo "Deploying site=$SITE env=$DEPLOY_ENV app_id=$APP_ID domain=$TARGET_DOMAIN"
 
 # Compute Cloudways application root
+# Build remote app root using remote $HOME to avoid hardcoding username/home
 if [[ -n "$APP_DIR" && "$APP_DIR" != "null" ]]; then
-  APP_ROOT="/home/${CLOUDWAYS_USER}/applications/$APP_DIR/public_html"
+  APP_ROOT="\$HOME/applications/$APP_DIR/public_html"
 else
-  APP_ROOT="/home/${CLOUDWAYS_USER}/applications/$APP_ID/public_html"
+  APP_ROOT="\$HOME/applications/$APP_ID/public_html"
 fi
 
 THEME_SLUG="marketing"
